c2c6c71cdfbff581609e67699770115c355b4685
jorgen <scubajorgen@gmail.com>
2024-06-11 20:13:35 +0200

    Update and optimisation

--- tileserver/styles/pdok/extract.sh
+++
@@ -1,1 +0,0 @@
-java -jar StyleConvert.jar EXTRACTLAYERS style_topnl.json.org layers3.csv
--- tileserver/styles/pdok/insert.sh
+++ tileserver/styles/pdok/insert.sh
@@ -1,1 +1,5 @@
-java -jar StyleConvert.jar INSERTLAYERS template.json layers.xlsx style_topnl.json
+#!/bin/bash
+# Script to merge CSV layers into the JSON file
+# Assumes StyleConvert.jar and StyleConvert.properties in this directory 
+
+java -jar StyleConvert.jar StyleConvertInsert.properties
